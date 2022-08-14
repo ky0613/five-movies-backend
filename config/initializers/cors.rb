233c1b46dd0743d239e2b9ecf2a52fb63a34fe94
ky0613 <kyutech0613@gmail.com>
2022-08-14 16:06:57 +0900

    fix: corsのoriginURLを訂正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'https://five-movies.net'
+    origins 'http://localhost:8080', 'https://www.five-movies.net/'
 
     resource "*",
       headers: :any,
