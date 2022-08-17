cfd9ea3b2581abc1c3cc5b6fb6691d8a881da541
ky0613 <kyutech0613@gmail.com>
2022-08-17 13:26:07 +0900

    add: バリデーション追加と同じuuidを許容しない設計に変更

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,21 +3,24 @@ class Api::V1::PostsController < ApplicationController
   def index
     posts = Post.order(created_at: :desc)
 
-    render json: posts
+    render json: posts, status: :ok
   end
 
   def create
-    created_image_path = CreateImage.new(params[:image_paths], params[:uuid]).create_image
-
     post = Post.new(post_params)
+
+    # 画像作成前にバリデーションを適用させる
+    return render json: { error: post.errors.full_messages }, status: :unprocessable_entity if post.invalid?
+
+    created_image_path = CreateImage.new(params[:image_paths], params[:uuid]).create_image
     post.image = File.open(created_image_path)
 
-    if post.save
+    if post.save(context: :post_image_setup)
       params[:movie_ids].each do |id|
         post.movies.create(movie_id: id)
       end
 
-      # 一時ファイルを削除
+      # 一時画像ファイルを削除
       File.delete(created_image_path)
 
       render json: post.uuid, status: :ok
@@ -30,7 +33,7 @@ class Api::V1::PostsController < ApplicationController
     post = Post.find_by(uuid: params[:uuid])
     movies = post.movies.all
     get_movies = GetMovieData.get_movies_data_from_tmdb(movies)
-    render json: { movies: get_movies, post_id: post.id }
+    render json: { movies: get_movies, post_id: post.id }, status: :ok
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   mount_uploader :image, PostImageUploader
   has_many :movies, dependent: :destroy
 
-  validates :uuid, :image, presence: true
+  validates :uuid,  presence: true, uniqueness: true
   validates :name, presence: true, length: {maximum: 10}
+  validates :image, presence: true, on: :post_image_setup
 
 end
